5cdd8c976188908d44a0b4f78d113bedd7fbb220
Sunny Mishra <sunnycoding712740@gmail.com>
2025-09-28 22:52:48 +0530

    Dom

---
+++ JS(FrontEnd)/index.js
@@ -0,0 +1,7 @@
+
+function abc() {
+    let a = document.getElementsByTagName("input")[0].value
+    // let a = document.getElementById("myInput").value
+    document.body.style.backgroundColor = a
+}
+
